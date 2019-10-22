ff9c6c5f9f7e47dfe6a5f81c9cac1805d7496953
Schittaci-air <barak.simon.as@gmail.com>
2019-10-22 16:55:52 +0200

    load app

---
+++ projects.js
@@ -0,0 +1,27 @@
+const projects = [
+  // PROJECT 1
+  {
+    name: "Anymade",
+    defaultMode: "slider",
+    images: [
+      "testVideo.mp4",
+      "image-5.jpg",
+      "image-6.jpg",
+      "image-7.jpg",
+      "image-8.jpg",
+      "image-9.jpg"
+    ]
+  },
+  // PROJECT 2
+  {
+    name: "First project",
+    defaultMode: "slider",
+    images: ["image-1.jpg", "image-2.png", "image-3.png", "image-4.jpg"]
+  },
+  // PROJECT 1
+  {
+    name: "Second project",
+    defaultMode: "slider",
+    images: ["image-7.jpg", "image-9.jpg", "image-8.jpg"]
+  }
+];
